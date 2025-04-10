930fd2a897de5e47a1be0bb28234579ef3555413
Hariharan <iamhariharan07@gmail.com>
2025-04-10 13:46:06 +0530

    minor changes

--- app/controllers/addm.js
+++ app/controllers/addm.js
@@ -1,10 +1,15 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-
 import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency';
+
 
 export default class AddmController extends Controller {
+  @tracked isInitialLoading = true;
+
+
+
   @service movie;
   @service router;
   @service flashMessages;
@@ -15,69 +20,58 @@ export default class AddmController extends Controller {
   genre = '';
   imdb = '';
   boxOffice = '';
-  isSaving = false;
-
-  // @action
-  // updateGenre(event) {
-  //   this.genre = event.target.value;
-  // }
 
   @tracked selectedGenre = null;
   @tracked genres = ['Action', 'Comedy', 'Drama', 'Sci-Fi', 'Horror', 'Thriller'];
 
   @action
-updateGenre(genre) {
-  this.selectedGenre = genre;
-  this.genre = genre; 
-}
-@action
-cancelForm() {
-  this.name = '';
-  this.year = '';
-  this.genre = '';
-  this.selectedGenre = null;
-  this.imdb = '';
-  this.boxOffice = '';
-}
-
-@action
-handleEnterKey(event) {
-  if (event.key === 'Enter') {
-    event.preventDefault();
-
-    let currentIndex = parseInt(event.target.getAttribute('data-input-index'));
-    let nextInput = document.querySelector(`[data-input-index="${currentIndex + 1}"]`);
-
-    if (nextInput && nextInput.classList.contains('ember-power-select-trigger')) {
-      nextInput.querySelector('input')?.focus();
-    } else if (nextInput) {
-      nextInput.focus();
-    } else {
-      console.log('last');
-    }
+  updateGenre(genre) {
+    this.selectedGenre = genre;
+    this.genre = genre; 
   }
-}
-
-@action
-handleEnterKeylast(event){
-  if (event.key === 'Enter'){
-  this.saveMovie(event);
-}
-}
-
 
   @action
-  async saveMovie(event) {
-    event.preventDefault();
-    if (this.isSaving) return;
+  cancelForm() {
+    this.name = '';
+    this.year = '';
+    this.genre = '';
+    this.selectedGenre = null;
+    this.imdb = '';
+    this.boxOffice = '';
+  }
 
-    // Validation
+  @action
+  handleEnterKey(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+
+      let currentIndex = parseInt(event.target.getAttribute('data-input-index'));
+      let nextInput = document.querySelector(`[data-input-index="${currentIndex + 1}"]`);
+
+      if (nextInput && nextInput.classList.contains('ember-power-select-trigger')) {
+        nextInput.querySelector('input')?.focus();
+      } else if (nextInput) {
+        nextInput.focus();
+      } else {
+        console.log('last');
+      }
+    }
+  }
+
+  @action
+  handleEnterKeylast(event) {
+    if (event.key === 'Enter') {
+      this.addMovieTask.perform(event);
+    }
+  }
+
+  @(task(function* (event) {
+    if (event) event.preventDefault();
+
     if (!this.name.trim() || !this.year || !this.genre || !this.imdb || !this.boxOffice) {
       this.flashMessages.warning('Please fill out all required fields!');
       return;
     }
-
-    this.isSaving = true;
 
     let movies = this.movie.movies;
     let lastId = movies.length > 0 ? movies[movies.length - 1].id : 0;
@@ -92,15 +86,11 @@ handleEnterKeylast(event){
     };
 
     try {
-      await this.movie.addMovie(newMovie);
+      yield this.movie.addMovie(newMovie);
       this.flashMessages.success('Movie added successfully!');
       this.router.transitionTo('movie-list');
     } catch (error) {
       this.flashMessages.danger(`Error saving movie: ${error.message}`);
-    } finally {
-      this.isSaving = false;
     }
-  }
-
-
+  }).drop()) addMovieTask;
 }
