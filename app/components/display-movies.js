28fd4607d33819dd8d6f9892cbb5565acd742b16
Hariharan <iamhariharan07@gmail.com>
2025-04-14 14:48:27 +0530

    minor change from the previous commit

--- app/components/display-movies.js
+++ app/components/display-movies.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { onKey, activated } from 'ember-keyboard';
+import { onKey } from 'ember-keyboard';
 
 export default class DisplayMoviesComponent extends Component {
   @tracked title = '';
